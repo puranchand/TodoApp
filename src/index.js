a1ccc154914fb209e35281d80f2280b5b5fb67f9
staffigarg <staffi.techindustan@gmail.com>
2017-09-05 12:37:32 +0530

    add best repo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './css/index.css';
 
 ReactDOM.render(
 	<div>
-		<h2>Hi I am A Component.</h2>
+		<h2>Hi I am A Component  hi</h2>
+		<h2>HI this is fully Functional component</h2>
+
 	</div>
 	, document.getElementById('root'));
